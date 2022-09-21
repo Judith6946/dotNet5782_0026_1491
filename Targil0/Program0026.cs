282a0339a010ca8e0126aed577fd99f8b512bfef
yt9074547@gmail.com <yt9074547@gmail.com>
2022-09-21 20:13:40 +0300

    Initial code + Welcome functions

---
+++ Targil0/Program0026.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+
+
+namespace Targil0
+{
+    partial class Program
+    {
+        public static void Main(string[] args)
+        {
+            Welcome0026();
+            Welcome1491();
+            Console.ReadKey();
+        }
+
+        private static void Welcome0026()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0} ,welcome to my first console application", name);
+        }
+
+
+        private static void Welcome1491()
+        {
+
+        }
+    }
+}
+
+
