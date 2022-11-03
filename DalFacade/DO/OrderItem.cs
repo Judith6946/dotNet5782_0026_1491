7fd8d81a5492f224b0a98872bc1035c499830df8
Judith <yt9074547@gmail.com>
2022-11-03 13:56:49 +0200

    tar1 stage2- building orderItem and product classes.

--- DalFacade/DO/OrderItem.cs
+++ DalFacade/DO/OrderItem.cs
@@ -1,9 +1,42 @@
 ﻿
+using System.Xml.Linq;
 
 namespace DO;
+
 /// <summary>
-/// Structure for OrderItem
+/// Structure for OrderItem.
 /// </summary>
 public struct OrderItem
 {
+    /// <summary>
+    /// Id of product.
+    /// </summary>
+    public int ProductId { get; set; }
+
+    /// <summary>
+    /// Id of order.
+    /// </summary>
+    public int OrderId { get; set; }
+
+    /// <summary>
+    /// Price of product.
+    /// </summary>
+    public double Price { get; set; }
+
+    /// <summary>
+    /// Quantity of product items in the order.
+    /// </summary>
+    public int Amount { get; set; }
+
+    /// <summary>
+    /// Report an OrderItem's description as a string. 
+    /// </summary>
+    /// <returns>A string representing an item in order.</returns>
+    public override string ToString() => $@"
+        Product ID={ProductId}, 
+        Order ID={OrderId}
+    	Price: {Price}
+    	Amount: {Amount}
+        ";
+
 }
