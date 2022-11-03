d6a2fdaa807f118cec3385eb7740f7dbeb699688
חיה פ <chaia.fogel@gmail.com>
2022-11-03 21:04:51 +0200

    Chaya - update (order)

--- DalFacade/DO/Order.cs
+++ DalFacade/DO/Order.cs
@@ -1,9 +1,55 @@
 ﻿
 
+using System.Diagnostics;
+
 namespace DO;
 /// <summary>
 /// Structure for Order
 /// </summary>
 public struct Order
 {
+    /// <summary>
+    /// Unique ID of order.
+    /// </summary>
+    public int ID { get; set; }
+
+    /// <summary>
+    /// Name of Customer.
+    /// </summary>
+    public string CustomerName { get; set; }
+
+    /// <summary>
+    /// Email of customer.
+    /// </summary>
+    public string CustomerEmail { get; set; }
+
+    /// <summary>
+    /// Adress of customer.
+    /// </summary>
+    public string CustomerAdress { get; set; }
+
+    /// <summary>
+    /// Date of order.
+    /// </summary>
+    public DateTime OrderDate { get; set; }
+
+    /// <summary>
+    /// Ship date of order.
+    /// </summary>
+    public DateTime ShipDate { get; set; }
+
+    /// <summary>
+    /// Delivery date of order.
+    /// </summary>
+    public DateTime DeliveryDate { get; set; }
+
+    public override string ToString() => $@"
+        Product ID={ID}, 
+        Order Name={CustomerName}
+    	Order Email={CustomerEmail}
+        Order Adress={CustomerAdress}
+        Order OrderDate={OrderDate}
+        Order ShipDate={ShipDate}
+        Order DeliveryDate={DeliveryDate}
+        ";
 }
