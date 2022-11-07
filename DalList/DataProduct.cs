d7146c8fae1183741e9a11e146122eda3e7a39d7
Judith <yt9074547@gmail.com>
2022-11-07 21:07:11 +0200

    ex1- adding docs

--- DalList/DataProduct.cs
+++ DalList/DataProduct.cs
@@ -3,41 +3,73 @@ using DO;
 
 namespace Dal;
 
+/// <summary>
+/// Access product data. 
+/// </summary>
 public class DataProduct
 {
-    
 
+    /// <summary>
+    /// Add product. 
+    /// </summary>
+    /// <param name="p">Product to be added</param>
+    /// <returns>Id of the new product.</returns>
     public int Add(Product p)
     {
         Random rn = new Random();
-        p.ID = rn.Next(100000, 999999);  //check if already exist!!!!!!!
-        DataSource.productsArr[DataSource.Config.productIndex] = p;
+        p.ID = rn.Next(100000, 999999);  
+        DataSource.addProduct(p);
         return p.ID;
     }
 
+    /// <summary>
+    /// Get a product by its id. 
+    /// </summary>
+    /// <param name="id">Id of product.</param>
+    /// <returns>Product object.</returns>
+    /// <exception cref="Exception">Thrown when the product cant be found.</exception>
     public Product Get(int id)
     {
-       return DataSource.productsArr.First(x => x.ID == id);
+        for (int i = 0; i < DataSource.Config.productIndex; i++)
+        {
+            if (DataSource.productsArr[i].ID == id)
+                return DataSource.productsArr[i];
+        }
+        throw new Exception("Cannot find this product.");
     }
 
-    public Product[] GetAll(int id)
+    /// <summary>
+    /// Get all of products. 
+    /// </summary>
+    /// <returns>Products array.</returns>
+    public Product[] GetAll()
     {
-        Product[] products = new Product[50];
-        Array.Copy(DataSource.productsArr, products, products.Length);
+        int size = DataSource.Config.productIndex;
+        Product[] products = new Product[size];
+        Array.Copy(DataSource.productsArr, products, size);
         return products;
     }
 
+    /// <summary>
+    /// Delete a product by its id.
+    /// </summary>
+    /// <param name="id">Id of product to be deleted</param>
     public void Delete(int id)
     {
         //how?????????
     }
 
+    /// <summary>
+    /// Update a product.
+    /// </summary>
+    /// <param name="p">Updated product.</param>
+    /// <exception cref="Exception">Thrown when product cant be found.</exception>
     public void Update(Product p)
     {
         bool found = false;
-        for(int i = 0; i < DataSource.productsArr.Length; i++)
+        for (int i = 0; i < DataSource.Config.productIndex; i++)
         {
-            if(DataSource.productsArr[i].ID == p.ID)
+            if (DataSource.productsArr[i].ID == p.ID)
             {
                 found = true;
                 DataSource.productsArr[i] = p;
@@ -54,3 +86,7 @@ public class DataProduct
 //איפה נשמרת הכמות שיש במערכים
 //איך מטפלים בחורים, מחיקה, הוספה וכו
 //איפה מקדמים את האינדקסים
+//למה המזהה האוטומוטי מאותחל??
+
+//זה בסדר שהוספנו מתודות הוספה??
+//מתודת הבדיקה, בסדר
--- DalList/DataSource .cs
+++ DalList/DataSource .cs
@@ -1,74 +1,107 @@
 ﻿
 using DO;
-using System.ComponentModel;
 
 namespace Dal;
 
+/// <summary>
+/// The class represents a temporary data-source.
+/// </summary>
 internal static class DataSource
 {
 
+    #region config_class
+    /// <summary>
+    /// A config class that manages the indexes & id's of the entities array. 
+    /// </summary>
     internal static class Config
     {
+        //arrays next available index
         internal static int orderIndex = 0;
         internal static int orderItemIndex = 0;
         internal static int productIndex = 0;
 
-        private static int orderLastId = 0;
-        private static int orderItemLastId = 0;
+        //arrays next id's
+        private static int orderLastId = 1000;
+        private static int orderItemLastId = 1000;
 
+        /// <summary>
+        /// A get property for the orderLastId field.
+        /// </summary>
+        public static int OrderLastId { get { return ++orderLastId; } }
 
-        public static int OrderLastId { get {  return ++orderLastId; } }
-
+        /// <summary>
+        /// A get property for the orderItemLastId field.
+        /// </summary>
         public static int OrderItemLastId { get { return ++orderItemLastId; } }
 
     }
 
+    #endregion
+
     private static readonly Random rn = new Random();
 
+    //entities arrays
     internal static Product[] productsArr = new Product[50];
     internal static Order[] ordersArr = new Order[100];
     internal static OrderItem[] orderItemsArr = new OrderItem[200];
 
+    #region ctor
+    /// <summary>
+    /// A static ctor, call the initializer method.
+    /// </summary>
     static DataSource()
     {
         s_Initialize();
     }
+    #endregion
 
+
+    #region Initializing_Methods
+
+
+
+    /// <summary>
+    /// Initialize the etities arrays. 
+    /// </summary>
     private static void s_Initialize()
     {
-        addProducts();
-        addOrders();
-        addOrderItems();
+        initializeProducts();
+        initializeOrders();
+        initializeOrderItems();
     }
 
-    private static void addProducts()
-    {
-        string[] productNames = new string[10]
-        {"great ring", "golden necklace", "rose bracelet", "goldfild bracelet",
-        "small silver earrings", "kids earrings", "personal design silver ring",
-        "Wedding ring", "Long decorated earring", "Personalized designer necklace" };
 
-        Enums.Category[] productsCategories = new Enums.Category[10]
-        {Enums.Category.ring,Enums.Category.necklace,Enums.Category.bracelet,Enums.Category.bracelet,
-        Enums.Category.earrings,Enums.Category.earrings,Enums.Category.ring,Enums.Category.ring,
-        Enums.Category.earrings,Enums.Category.necklace};
+    /// <summary>
+    /// Initialize the products array. 
+    /// </summary>
+    private static void initializeProducts()
+    {
+        string[] productNames = new string[10] { "great ring", "golden necklace", "rose bracelet", "goldfild bracelet", "small silver earrings", "kids earrings", "personal design silver ring", "Wedding ring", "Long decorated earring", "Personalized designer necklace" };
+        Enums.Category[] productsCategories = new Enums.Category[10] { Enums.Category.ring, Enums.Category.necklace, Enums.Category.bracelet, Enums.Category.bracelet, Enums.Category.earrings, Enums.Category.earrings, Enums.Category.ring, Enums.Category.ring, Enums.Category.earrings, Enums.Category.necklace };
+
 
         for (int i = 0; i < 10; i++)
         {
-            productsArr[i] = new Product();
-            productsArr[i].ID = rn.Next(100000, 999999);
-            productsArr[i].Name = productNames[i];
-            productsArr[i].Category = productsCategories[i];
-            productsArr[i].Price = rn.Next(100, 500);
-            productsArr[i].InStock = rn.Next(0, 30);
+            Product p = new Product();
+            p.ID = rn.Next(100000, 999999);
+            p.Name = productNames[i];
+            p.Category = productsCategories[i];
+            p.Price = rn.Next(100, 500);
+            p.InStock = rn.Next(0, 30);
 
-            if (i == 0) productsArr[i].InStock = 0;
+            if (i == 0) p.InStock = 0;
+
+            addProduct(p);
         }
 
-        
+
     }
 
-    private static void addOrders()
+
+    /// <summary>
+    /// Initialize the orders array. 
+    /// </summary>
+    private static void initializeOrders()
     {
         string[] customerNames = new string[10] { "Yehudit", "Chaya", "Sara", "David", "Moshe", "Tovi", "Adina", "Avi", "Dan", "Miri" };
         string[] customerEmails = new string[10] { "yt9074547@gmail.com", "Chaya9@gmail.com", "Sara@gmail.com", "David@gmail.com", "Moshe@gmail.com", "Tovi@gmail.com", "Adina@gmail.com", "Avi@gmail.com", "Dan@gmail.com", "Miri@gmail.com" };
@@ -76,30 +109,104 @@ internal static class DataSource
 
         for (int i = 0; i < 20; i++)
         {
-            ordersArr[i].ID = Config.OrderLastId; 
-            ordersArr[i].CustomerName = customerNames[i % 10];
-            ordersArr[i].CustomerEmail = customerEmails[i % 10];
-            ordersArr[i].CustomerAdress = customerAdress[i % 10];
-            ordersArr[i].OrderDate = DateTime.MinValue;
+            Order order = new Order();
+            order.ID = Config.OrderLastId;
+            order.CustomerName = customerNames[i % 10];
+            order.CustomerEmail = customerEmails[i % 10];
+            order.CustomerAdress = customerAdress[i % 10];
+            order.OrderDate = DateTime.MinValue;
             TimeSpan tms = new TimeSpan(rn.Next(1, 10), rn.Next(0, 24), rn.Next(0, 60), rn.Next(0, 60));
-            ordersArr[i].ShipDate = DateTime.MinValue + tms;
+            order.ShipDate = DateTime.MinValue + tms;
             tms.Add(new TimeSpan(rn.Next(1, 10), rn.Next(0, 24), rn.Next(0, 60), rn.Next(0, 60)));
-            ordersArr[i].DeliveryDate = ordersArr[i].ShipDate + tms;
+            order.DeliveryDate = order.ShipDate + tms;
+
+            addOrder(order);
         }
     }
 
-    private static void addOrderItems()
+
+    /// <summary>
+    /// Initialize the orderItems array. 
+    /// </summary>
+    private static void initializeOrderItems()
     {
         for (int i = 0; i < 40; i++)
         {
-            orderItemsArr[i] = new OrderItem();
-            orderItemsArr[i].ID=Config.OrderItemLastId;
-            orderItemsArr[i].OrderId = ordersArr[i % 20].ID;
-            orderItemsArr[i].ProductId = productsArr[i % 10].ID;
-            orderItemsArr[i].Price = productsArr[i % 10].Price;
-            orderItemsArr[i].Amount = rn.Next(1, 5);
+            OrderItem orderItem = new OrderItem();
+            orderItem.ID = Config.OrderItemLastId;
+            orderItem.OrderId = ordersArr[i % 20].ID;
+            orderItem.ProductId = productsArr[i % 10].ID;
+            orderItem.Price = productsArr[i % 10].Price;
+            orderItem.Amount = rn.Next(1, 5);
 
+            addOrderItem(orderItem);
         }
     }
+
+    #endregion
+
+
+    #region Adding_Methods
+
+
+    /// <summary>
+    /// Add a product to the products array.
+    /// </summary>
+    /// <param name="product">The product to be added</param>
+    internal static void addProduct(Product product)
+    {
+        if (!validateProductId(product.ID))
+            throw new Exception("The product is already exist");
+
+        int i = Config.productIndex;
+        productsArr[i] = product;
+        Config.productIndex++;
+    }
+
+
+    /// <summary>
+    /// Add an order to the orders array.
+    /// </summary>
+    /// <param name="order">The order to be added</param>
+    internal static void addOrder(Order order)
+    {
+        int i = Config.orderIndex;
+        ordersArr[i] = order;
+        Config.orderIndex++;
+    }
+
+
+    /// <summary>
+    /// Add an orderItem to the orderItems array.
+    /// </summary>
+    /// <param name="orderItem">The orderItem to be added</param>
+    internal static void addOrderItem(OrderItem orderItem)
+    {
+        int i = Config.orderItemIndex;
+        orderItemsArr[i] = orderItem;
+        Config.orderItemIndex++;
+    }
+
+
+    #endregion
+
+
+    #region validation
+    /// <summary>
+    /// Validate an id of a new product.
+    /// </summary>
+    /// <param name="id">The id to be validated</param>
+    /// <returns>A boolean variable that determines whether the id is ok</returns>
+    internal static bool validateProductId(int id)
+    {
+        for(int i = 0; i < Config.productIndex; i++)
+        {
+            if (productsArr[i].ID == id)
+                return false;
+        }
+        return true;
+    }
+
+    #endregion
 
 }
